2d147508dd9a13cc3f4178a0122a0734e4f21df1
Allen McNichols <allen.r.mcnichols@gmail.com>
2018-04-08 03:50:28 -0700

    Category and Tags Fix
    Finally able to use Categories and Tags!

--- transponder-admin/transponder-posts.php
+++ transponder-admin/transponder-posts.php
@@ -1,39 +1,30 @@
 <?php
-add_action('init','create_posttype');
-function create_posttype() {
-	register_post_type( 'Resource Suggestions',
-		array(
-			'labels' => array(
-				'name' => __('Resource Suggestions'),
-				'singular_name' => __('Resource Suggestion')
-			),
-			'public' => true,
-			'publicly_queryable' => true,
-			'query_var' => true,
-			'capability_type' => 'post',
-			'show_in_menu' => true,
-			'menu_position' => 1,
-			'supports' => array('title', 'editor', 'thumbnail', 'custom-fields', 'excerpt', 'tags'),
-			'has_archive' => true,
-			'rewrite' => array('slug' => 'resource-suggestions'),
-			'show_ui' => true,
-			'menu_icon' => 'dashicons-location-alt',
-			'taxonomies' => array('category', 'post_tag')
-		)
+function create_resources_post_type() {
+	$labels = array(
+		'name' => 'Resource Suggestions',
+		'singular_name' => 'Resource Suggestion'
 	);
+	$args = array (
+		'labels' => $labels,
+		'public' => true,
+		'has_archive' => true,
+		'publicly_queryable' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'heirarchical' => false,
+		'menu_icon' => 'dashicons-location-alt',
+		'suppports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'thumbnail',
+			'revisions',
+	),
+	'taxonomies' => array('category','post_tag'),
+	'menu_position' => 1
+	);
+	register_post_type('resourcesuggestions',$args);
 }
-add_action( 'init', 'create_client_tax' );
-function create_client_tax() {
-    register_taxonomy( 
-            'client_tag', //your tags taxonomy
-            'Resource Suggestions',  // Your post type
-            array( 
-                'hierarchical'  => false, 
-                'label'         => __( 'Tags', CURRENT_THEME ), 
-                'singular_name' => __( 'Tag', CURRENT_THEME ), 
-                'rewrite'       => true, 
-                'query_var'     => true 
-            )  
-        );
-}
+add_action('init','create_resources_post_type');
 ?>
