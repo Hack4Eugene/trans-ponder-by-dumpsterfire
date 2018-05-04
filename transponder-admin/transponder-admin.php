ebb86896d8af1d7cb63e534babc0d99b0847ffbe
CodeGold <codegold79@gmail.com>
2018-05-04 07:26:47 -0700

    enable admin and volunteer pending pages updates

--- transponder-admin/transponder-admin.php
+++ transponder-admin/transponder-admin.php
@@ -8,9 +8,9 @@
 		
 	/*	Create a custom table to store all submissions after review
 	*	This table contains every field available on the admin 
-	*	view form
+    *	view form. It is the form that helps to determine 
+    *   which rows in the Volunteer and Admin Pending Review tables are displayed 
 	*/	
-	
     function create_providers_table() 
     {
         global $wpdb;
@@ -21,7 +21,7 @@
         $sql = 'CREATE TABLE IF NOT EXISTS ' . $table_name . ' (
             id int(20) unsigned NOT NULL AUTO_INCREMENT,
             lead_id int(20) unsigned NOT NULL,
-            is_provider_submitted tinytext,
+            is_provider_submitted text,
             service_type text,
             medical_type text,
             mental_type text,
@@ -39,10 +39,9 @@
             provider_email text,
             provider_url text,
             submitter_feedback text,
-            experience_rating tinyint(10),
+            experience_rating tinytext,
             is_trans_experienced tinytext,
             accepts_ohp tinytext,
-            ohp_providers text,
             accepts_private_insurance tinytext,
             accepted_insurance text,
             accepts_medicare tinytext,
@@ -51,13 +50,20 @@
             is_awareness_trained tinytext,
             awareness_training_date date,
             awareness_trainer text,
-            required_trainees text,
+            required_trainee_office text,
+            required_trainee_caseWkr text,
+            required_trainee_doctor text,
+            required_trainee_admStaff text,
+            required_trainee_allStaff text,
+            required_trainee_other text,
             has_more_than_m_f tinytext,
             options_other_than_m_f text,
             pronoun_requested tinytext,
             preferred_name_requested tinytext,
             can_prescribe_hormones tinytext,
-            letters_of_assistance text,
+            assist_letter_hormones text,
+            assist_letter_identDocChng text,
+            assist_letter_gdrAffSurg text,
             additional_comments text,
             is_review_ready tinytext,
             admin_first_name text,
@@ -71,6 +77,8 @@
             post_body text,
             post_tags text,
             post_category text,
+            comm_id text,
+            vol_id text,
 
             PRIMARY KEY  (id),
             KEY lead_id (lead_id)
@@ -79,25 +87,36 @@
             require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
             dbDelta($sql);
     }
+
+    // When plugin is activated check and create the table if necessary
+	register_activation_hook(__FILE__, 'create_providers_table'); 
     
-	register_activation_hook(__FILE__, 'create_providers_table'); // When plugin is activated check and create the table if necessary
-	add_action('admin_menu', 'transponder_admin_menu'); // Add Trans*ponder to the WordPress admin menu
-	add_action('admin_enqueue_scripts','shinyStuff'); // Apply a bit of polish and make things display a little better
-	function shinyStuff() {
-		// shinyStuff brings in our stylesheet so we can style the admin section without having to do a bunch of inline shenanigans
+    // Add Trans*ponder to the WordPress admin menu
+    add_action('admin_menu', 'transponder_admin_menu');
+    
+    // Apply a bit of polish and make things display a little better
+    add_action('admin_enqueue_scripts','shinyStuff'); 
+    
+    /* shinyStuff brings in our stylesheet so we can style the admin section without having to do a bunch of inline shenanigans */ 
+    function shinyStuff() 
+    {
 		wp_register_style('transponder-admin',plugins_url('style.css',__FILE__ ));
 		wp_enqueue_style('transponder-admin', get_stylesheet_uri() );
-	}
-	function transponder_admin_menu() {
-		// transponder_admin_menu sets up the admin menu with 2 option. The Admin option is only available to Administrators and Volunteers can't access this
+    }
+    
+    /* transponder_admin_menu sets up the admin menu with 2 option. The Admin option is only available to Administrators and Volunteers can't access this */
+    function transponder_admin_menu() 
+    {
 		add_menu_page( 'Trans*ponder Posts', 'Trans*ponder', 'edit_posts', 'transponder-admin', 'transponder_pending', plugins_url('transponder-admin/includes/images/pluginicon.png'), 0 );
 		add_submenu_page('transponder-admin','Pending Submissions','Pending','edit_posts', 'transponder-admin','transponder_pending');
 		add_submenu_page('transponder-admin','Admin','Admin','edit_users', 'transponder-admin-settings','transponder_vol');
-	}
+    }
+    
 	/*
 	*	 Setup Custom Post Type for Approved Submissions
 	*/
-	function create_resources_post_type() {
+    function create_resources_post_type() 
+    {
 		$labels = array(
 			'name' => 'Resource Suggestions',
 			'singular_name' => 'Resource Suggestion'
@@ -123,21 +142,28 @@
 		'menu_position' => 1
 		);
 		register_post_type('resourcesuggestions',$args);
-	}
-	add_action('init','create_resources_post_type');
-	function get_taxonomy_queries( $query ) {
+    }
+    
+    add_action('init','create_resources_post_type');
+    
+    function get_taxonomy_queries( $query ) 
+    {
 		if ( ( $query->is_category() || $query->is_tag() )
 			&& $query->is_main_query() ) {
 				$query->set( 'post_type', array( 'post', 'resourcesuggestions' ) );
 		}
-	}
+    }
+    
 	add_action( 'pre_get_posts', 'get_taxonomy_queries' );
-	register_taxonomy_for_object_type('category', 'resourcesuggestions');
+    
+    register_taxonomy_for_object_type('category', 'resourcesuggestions');
+
 	/*
 	*	This function is under development
 	*	Scope: Administrators should be able to add resources without review
-	*	Acceptance Criteria: Admins only, Check if we are editing an existing form or creating a new one, provide admin view of form
-	function transponder_admin() {
+    *	Acceptance Criteria: Admins only, Check if we are editing an existing form 
+    *   or creating a new one, provide admin view of form 
+    function transponder_admin() {
 		
 		if(isset($_GET['create'])) {
 
@@ -149,19 +175,30 @@
 		}
 	}
 	*/
-	function transponder_pending() {
-		/*	
-		*	transponder_pending is the Volunteer review section, admins can use this to look at new submissions as well, that allows
-		*	the review workflow to proceed forward and be vetted before an admin is needed to approve the submission
-		*/	
+    
+    /*	
+    *	transponder_pending is the Volunteer review section, admins can use this 
+    *   to look at new submissions as well, that allows
+    *	the review workflow to proceed forward and be vetted before an admin is 
+    *   needed to approve the submission
+    */	
+    function transponder_pending() 
+    {
 		$entries = GFFormsModel::get_leads(1);
 		if(isset($_GET['id'])){
 			$id = $_GET['id'];
 			review_submission($id);
 		} else {
-			echo "<h1>Pending Submissions</h1>";
-			$entries = GFFormsModel::get_leads(1);	// This will grab all of the entries from the community based form
-			echo "<script> window.leadData = ".json_encode($entries).";</script>"; // This outputs a json string we can use on the front end
+            echo "<h1>Pending Submissions Ready for Volunteers to Review</h1>";
+            
+            // This will grab all of the entries from the community based form
+            $entries = GFFormsModel::get_leads(1);	
+
+            // determine if each entry is visible or not.
+            addIsVisibleToEntries('comm', $entries);
+
+            // This outputs a json string we can use on the front end
+            echo "<script> window.leadData = ".json_encode($entries).";</script>"; 
 			echo "<div id='formEntries'></div>";
 			?>
 		<!--
@@ -174,8 +211,8 @@
 			document.addEventListener("DOMContentLoaded", function(){
 				drawTable(leadData);
 			});
+
 			function drawTable(data) {
-				
 				var heading = jQuery("<tr />");
 				jQuery("#mytable").append(heading);
 				heading.append(jQuery("<td class='heading'>Submitted At</td>"));
@@ -184,11 +221,15 @@
 				heading.append(jQuery("<td class='heading'>Provider Name</td>"));
 				heading.append(jQuery("<td class='heading'>Review</td>"));
 				for (var i = 0; i < data.length; i++) {
-					drawRow(data[i]);
+                    // draw row if "is_review_ready" = "No" on the volunteer's form.
+					if (data[i]['visible'] !== false) {
+                        drawRow(data[i]);
+                    }
 				}
 			}
+
 			function drawRow(rowData) {
-				console.log(rowData);
+				//console.log(rowData);
 				var row = jQuery("<tr />");
 				jQuery("#mytable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
 				row.append(jQuery("<td>" + rowData['date_created'] + "</td>"));
@@ -201,18 +242,25 @@
 		</script>
 		<?php
 		}
-	}
+    }
+    
+    /*	
+    *	transponder_vol is the admin view and populates the form using the 
+    *   information entered by the community and our volunteer
+    */	
 	function transponder_vol() {
-		/*	
-		*	transponder_vol is the admin view and populates the form using the information entered by the community and our volunteer
-		*/	
-		$entries = GFFormsModel::get_leads(3);
+        $entries = GFFormsModel::get_leads(3);
+
 		if(isset($_GET['id'])){
 			$id = $_GET['id'];
 			review_vol($id);
 		} else {
-			echo "<h1>Pending Submissions</h1>";
-			$entries = GFFormsModel::get_leads(3);
+			echo "<h1>Pending Submissions for Final Review by Admin</h1>";
+            $entries = GFFormsModel::get_leads(3);
+
+            // determine if each entry should be visible or not
+            addIsVisibleToEntries('vol', $entries);
+
 			echo "<script> window.leadData = ".json_encode($entries).";</script>";
 			echo "<div id='formEntries'></div>";
 			?>
@@ -223,42 +271,53 @@
 			document.addEventListener("DOMContentLoaded", function(){
 				drawTable(leadData);
 			});
-			function drawTable(data) {
-				
-				var heading = jQuery("<tr />");
-				jQuery("#mytable").append(heading);
-				heading.append(jQuery("<td class='heading'>Submitted At</td>"));
-				heading.append(jQuery("<td class='heading'>Type of Services</td>"));
-				heading.append(jQuery("<td class='heading'>Provider Type</td>"));
-				heading.append(jQuery("<td class='heading'>Provider Name</td>"));
-				heading.append(jQuery("<td class='heading'>Review</td>"));
-				for (var i = 0; i < data.length; i++) {
-					drawRow(data[i]);
-				}
-			}
-			function drawRow(rowData) {
-				console.log(rowData);
-				var row = jQuery("<tr />");
-				jQuery("#mytable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
-				row.append(jQuery("<td>" + rowData['date_created'] + "</td>"));
-				row.append(jQuery("<td>" + rowData[2] + "</td>"));
-				row.append(jQuery("<td>" + rowData[3] + "</td>"));
-				row.append(jQuery("<td>" + rowData[7] + "</td>"));
-				row.append(jQuery("<td><a class='button' href='admin.php?page=transponder-admin-settings&id="+rowData['id']+"'>Start</a></td>"));
-			}
+
+            function drawTable(data) 
+            {
+                
+                var heading = jQuery("<tr />");
+                jQuery("#mytable").append(heading);
+                heading.append(jQuery("<td class='heading'>Submitted At</td>"));
+                heading.append(jQuery("<td class='heading'>Type of Services</td>"));
+                heading.append(jQuery("<td class='heading'>Provider Type</td>"));
+                heading.append(jQuery("<td class='heading'>Provider Name</td>"));
+                heading.append(jQuery("<td class='heading'>Review</td>"));
+                for (var i = 0; i < data.length; i++) {
+                    if(data[i]['visible'] !== false) {
+                        drawRow(data[i]);
+                    }
+                }
+            }
+
+            function drawRow(rowData) 
+            {
+                // console.log(rowData);
+                var row = jQuery("<tr />");
+                jQuery("#mytable").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+                row.append(jQuery("<td>" + rowData['date_created'] + "</td>"));
+                row.append(jQuery("<td>" + rowData[2] + "</td>"));
+                row.append(jQuery("<td>" + rowData[3] + "</td>"));
+                row.append(jQuery("<td>" + rowData[7] + "</td>"));
+                row.append(jQuery("<td><a class='button' href='admin.php?page=transponder-admin-settings&id="+rowData['id']+"'>Start</a></td>"));
+            }
 
 		</script>
 		<?php
 		}
-	}
-	function review_submission($id) {
-		/*	
-		*	This is our volunteer view, this allows them to review the submission we received and fill out more information as they talk with the provider
-		*/	
-		if(strcmp($id,'-1') == 0) { // There should not be any negative form id's 
+    }
+    
+    /*	
+    *	This is our volunteer view, this allows them to review the submission 
+    *   we received and fill out more information as they talk with the provider
+    */	
+    function review_submission($id) 
+    {
+        // There should not be any negative form ids
+		if(strcmp($id,'-1') == 0) {  
 			return "Nothing to review";
 		}
-		$entries = GFFormsModel::get_leads(1);
+        $entries = GFFormsModel::get_leads(1);
+
 		foreach($entries as $entry) {
 			$lead_id = $entry['id'];
 			$lead = RGFormsModel::get_lead( $lead_id ); 
@@ -274,17 +333,24 @@
 				var fieldId = key.replace('.', '_');
 				jQuery('#input_3_'+fieldId).val(leadData[key]);
 			})
-		</script>
+            // store the community submission entry id (id => comm_id)
+            jQuery('#input_3_44').val(leadData['id']);
+		
+        </script>
 		<?php
-	}
-	function review_vol($id) {
-		/*	
-		*	This is our admin review view that will be populated with the community and volunteer additions if it passed review
-		*/	
+    }
+    
+    /*	
+    *	This is our admin review view that will be populated with the community 
+    *   and volunteer additions if it passed review
+    */	
+    function review_vol($id) 
+    {
 		if(strcmp($id,'-1') == 0) {
 			return "Nothing to review";
 		}
-		$entries = GFFormsModel::get_leads(3);
+        $entries = GFFormsModel::get_leads(3);
+
 		foreach($entries as $entry) {
 			$lead_id = $entry['id'];
 			$lead = RGFormsModel::get_lead( $lead_id ); 
@@ -303,28 +369,74 @@
 			jQuery('#input_5_53').val(leadData[14]);
 			jQuery('#input_5_54').val(leadData[7]);
 			jQuery('#input_5_55').val(leadData[2]);
+
+            // store the community submission entry id (id => comm_id) as well as the volunteer submission entry id (id => vol_id).
+			jQuery('#input_5_57').val(leadData[44]);
+			jQuery('#input_5_58').val(leadData['id']);
+            //console.log('the admin form should obtain comm_id as ' + leadData[44] + ' and vol_id should be ' . leadData['id']);
 		</script>
 		<?php
-	}
-	/*	
-	*	This function will be used to dump the entire form into a table so we can log all submission and view them later as needed
-	*	
-	function reviewed5($entry, $form) {
-		$form_id = $form['id'];
-		$send_it = "INSERT INTO wp_a3t9xkcyny_serviceProviders  (`id`,`lead_id`,`is_provider_submitted`,`service_type`,`medical_type`,`mental_type`,`surgical_type`,`bodywork_type`,`provider_name`,`office_name`,`provider_address`,`provider_address_2`,`provider_city`,`provider_state`,`provider_zip`,`provider_country`,`provider_phone`,`provider_email`,`provider_url`,`submitter_feedback`,`experience_rating`,`is_trans_experienced`,`accepts_ohp`,`accepts_private_insurance`,`insured_providers`,`accepts_medicare`,`accepts_scale_payments`,`scale_payment_desc`,`is_awareness_trained`,`awareness_training_date`,`awareness_trainer`,`required_trainees`),`has_more_than_m_f`,`pronoun_requested`,`preferred_name_requested`,`can_prescribe_hormones`,`letters_of_assistance`,`additional_comments`) VALUES (";
-		$entries = GFFormsModel::get_leads($form);
-		foreach($entries as $entry) {
-			$lead_id = $entry['id'];
-			$lead = RGFormsModel::get_lead( $lead_id ); 
-			if($entry['id'] == $_GET['id']) {
-				foreach($entry as $value) {
-					$send_it .= $value;
-				}
-			}
-		}
-		$send_it .= ");";
-		var_dump($send_it);
-	}
-	*/
-	//add_action( 'gform_post_submission_5', 'reviewed5', 10, 2 );
+    }
+    
+    /*
+     * Given the form type (volunteer or admin), determine
+     * if it should be on the Volunteer's or Admin's Pending Lists or no list.
+     */
+    function addIsVisibleToEntries($formType, & $entriesArray)
+    {
+        foreach ($entriesArray as &$entry) {
+            if ($formType === 'comm') {
+                $entry['visible'] = isCommVisible($entry['id']);
+            }
+
+            if ($formType === 'vol') {
+                $entry['visible'] = isVolVisible($entry['id']);
+            }
+        }
+    }
+
+    /*
+     * Community submited rows should only be visible to volunteers
+     * if is_review_ready = 'no', otherwise, it should be on
+     * the admin's pending review page
+     */
+    function isCommVisible ($entryID)
+    {
+        global $wpdb; 
+        $tableName = $wpdb->prefix . 'providers_table';
+
+        $data=$wpdb->get_results(
+                'SELECT is_review_ready  FROM ' . $tableName . ' WHERE comm_id = ' . $entryID 
+            );
+            
+            if ($data[0]->is_review_ready === 'Yes') {
+                return false;
+            } 
+            
+            return true;
+    }
+
+    /*
+     * volunteer submitted rows should only be visible to admins if
+     * is_review_ready = 'yes' AND (publish_to_web = 'no' OR empty) AND
+     * (is_followup_needed = 'yes' OR empty AND archive_listing = 'no')
+     */
+    function isVolVisible ($entryID)
+    {
+        global $wpdb; 
+        $tableName = $wpdb->prefix . 'providers_table';
+
+        $data=$wpdb->get_results(
+                'SELECT is_review_ready, publish_to_web, archive_listing, followup_needed FROM ' . $tableName . ' WHERE vol_id = ' . $entryID 
+            );
+
+            if (
+                $data[0]->is_review_ready === 'No'
+                || $data[0]->publish_to_web === 'Yes' 
+                || ($data[0]->archive_listing === 'Yes' && $data[0]->followup_needed === 'No')
+            ) {
+                return false;
+            } 
+            return true;
+    }
 ?>
